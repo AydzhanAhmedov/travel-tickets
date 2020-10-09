17ecf5d06872ec8ac974296f1151b0d83eac71c6
Nikolay <nikolay.ivanov@mentormate.com>
2020-10-09 15:10:07 +0300

    TRAV-1: persistence.xml added, hibernate configured

---
+++ src/main/java/bg/tuvarna/traveltickets/common/Constants.java
@@ -0,0 +1,11 @@
+package bg.tuvarna.traveltickets.common;
+
+public final class Constants {
+
+    public static final String PERSISTENT_UNIT_NAME = "travel-ticket-persistence-unit";
+
+    private Constants() {
+        throw new RuntimeException(getClass().toString() + " cannot be instantiated!");
+    }
+
+}
